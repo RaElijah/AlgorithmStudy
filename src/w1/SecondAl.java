6c82bd9c4e52368bc59bd09f9f3329ee54a2a63c
RaElijah <140477753+RaElijah@users.noreply.github.com>
2023-11-29 18:14:59 +0900

    init

---
+++ src/w1/SecondAl.java
@@ -0,0 +1,21 @@
+package w1;
+
+// 백준 문제 링크 : https://www.acmicpc.net/problem/1001
+
+/*
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+ */
+
+import java.util.Scanner;
+
+public class SecondAl {
+    public static void main(String[] args) {
+        // 출력하기 위해 Scanner 이용
+        Scanner sc = new Scanner(System.in);
+        // 두 정수 A, B
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a - b);
+    }
+}
