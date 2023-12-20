d594d629289a31fbb09f2b163b5c44ed0e010e6b
RaElijah <140477753+RaElijah@users.noreply.github.com>
2023-12-20 17:03:43 +0900

    algorithm study

---
+++ src/w4/FirstAl.java
@@ -0,0 +1,25 @@
+package w4;
+
+// 백준 문제 링크: https://www.acmicpc.net/problem/25083
+
+/*
+아래 예제와 같이 새싹을 출력하시오.
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+public class FirstAl {
+    public static void main(String[] args) {
+        // escape sequence \을 이용하여 \\, \" 을 쓴다.
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
---
+++ src/w4/ThirdAl.java
@@ -0,0 +1,60 @@
+package w4;
+
+// 백준 문제 링크: https://www.acmicpc.net/problem/2444
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력
+5
+
+예제 출력
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+ */
+public class ThirdAl {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        char[] star = new char[2 * n - 1];
+
+        for(int j = 0; j < n; j++) {
+            for(int i = n - 1; i < n; i++) {
+                star[n - 1 - j] = '*';
+                star[n - 1 + j] = '*';
+            }
+            for(int i = 0; i <star.length; i++) System.out.print(star[i]);
+            System.out.print("\n");
+        }
+
+        for(int j = n; j > 1; j--) {
+            for(int i = n - 1; i < n; i++) {
+                star[n - j] = ' ';
+                star[n + j - 2] = ' ';
+            }
+            for(int i = 0; i <star.length; i++) System.out.print(star[i]);
+            System.out.print("\n");
+        }
+
+    }
+
+}
